c937c15c285c061447470dc2bdafec912f66f02b
JB0129 <rltkqja1@gmail.com>
2024-01-29 23:58:47 +0900

    키보드 생성

---
+++ client/src/components/Keyboard/Keyboard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Keyboard: React.FC = () => {
+  const topKey = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "←"];
+  const middleKey = ["a", "s", "d", "f", "g", "h", "j", "k", "l", "Enter"];
+  const bottomKey = ["z", "x", "c", "v", "b", "n", "m"];
+
+  return (
+    <div>
+      {<div>{<div></div>}</div>}
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Keyboard;
