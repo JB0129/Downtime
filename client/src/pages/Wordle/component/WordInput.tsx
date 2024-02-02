1632ab638b4233df9e2a01423b4ffc739201a882
JB0129 <rltkqja1@gmail.com>
2024-02-02 23:27:38 +0900

    Wordle 기본 규칙 적용 완료

--- client/src/pages/Wordle/component/WordInput.tsx
+++ client/src/pages/Wordle/component/WordInput.tsx
@@ -7,30 +7,36 @@ interface OwnProps {
 }
 
 const WordInput: React.FC<OwnProps> = ({ solve, handleMoreChance }) => {
-  // useEffect(() => {
-  //   if (complete) {
-  //     const answer = Object.values(isWord).join("");
-  //     if (solve !== answer) {
-  //       handleMoreChance();
-  //     } else {
-  //       alert("정답입니다!");
-  //     }
-  //   }
-  // }, [complete]);
-
   const [isWord, setWord] = useState<string[]>([]);
+  const [complete, setComplete] = useState<boolean>(false);
 
   useEffect(() => {
-    window.onkeydown = (e) => {
-      if (/[a-zA-Z]/.test(e.key) && e.key.length === 1 && isWord.length < 5) {
-        console.log(e.key);
-        setWord((isWord) => [...isWord, e.key.toUpperCase()]);
-      }
-      if (e.key === "Backspace" && isWord.length > 0) {
-        setWord((isWord) => isWord.slice(0, isWord.length - 1));
-      }
-    };
+    if (!complete) {
+      window.onkeydown = (e) => {
+        if (/[a-zA-Z]/.test(e.key) && e.key.length === 1 && isWord.length < 5) {
+          console.log(e.key);
+          setWord((isWord) => [...isWord, e.key.toUpperCase()]);
+        }
+        if (e.key === "Backspace" && isWord.length > 0) {
+          setWord((isWord) => isWord.slice(0, isWord.length - 1));
+        }
+        if (e.key === "Enter" && isWord.length === 5) {
+          setComplete(true);
+        }
+      };
+    }
   }, [isWord]);
+
+  useEffect(() => {
+    if (complete) {
+      const answer = isWord.join("");
+      if (solve.toUpperCase() !== answer) {
+        handleMoreChance();
+      } else {
+        alert("정답입니다!");
+      }
+    }
+  }, [complete]);
 
   return (
     <div>
