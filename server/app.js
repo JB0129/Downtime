1859b9dfb91fa7f25f50900ad15ac6358e57e9bf
JB0129 <rltkqja1@gmail.com>
2024-02-10 23:52:36 +0900

    express 및 cors 설치

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+// TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+app.use(express.json({ strict: false }));
+
+const port = 8080;
+
+app.get("", (req, res) => {
+  res.status(200).send("downtime server");
+});
+
+const server = app.listen(port, () => {
+  console.log(`[RUN] Downtime Server... | http://localhost:${port}`);
+});
